3ea6aeae4003d1fd751f9fea4fb03ea3bb4433a5
manivasagan <80743457+mani-bca@users.noreply.github.com>
2025-04-30 21:07:47 +0530

    Update variables.tf

--- env/dev/variables.tf
+++ env/dev/variables.tf
@@ -1,57 +1,62 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
+  default     = "us-west-2"
 }
-
-
-# VPC variables removed as we're using the default VPC
-
 
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "eks-cluster"
+  default     = "my-eks-cluster"
 }
 
 variable "cluster_version" {
-  description = "Kubernetes version for the EKS cluster"
+  description = "Kubernetes version to use for the EKS cluster"
   type        = string
   default     = "1.27"
 }
 
 variable "instance_type" {
-  description = "EC2 instance type for the EKS worker nodes"
+  description = "Instance type for the worker nodes"
   type        = string
   default     = "t3.medium"
 }
 
 variable "min_size" {
-  description = "Minimum number of worker nodes"
+  description = "Minimum size of the worker node group"
   type        = number
-  default     = 2
+  default     = 1
 }
 
 variable "max_size" {
-  description = "Maximum number of worker nodes"
-  type        = number
-  default     = 5
-}
-
-variable "desired_size" {
-  description = "Desired number of worker nodes"
+  description = "Maximum size of the worker node group"
   type        = number
   default     = 3
 }
 
-variable "disk_size" {
-  description = "Disk size in GB for worker nodes"
+variable "desired_size" {
+  description = "Desired size of the worker node group"
   type        = number
-  default     = 50
+  default     = 2
 }
 
-variable "environment" {
-  description = "Environment tag for all resources"
+variable "key_name" {
+  description = "Key pair name to use for the instances"
   type        = string
-  default     = "dev"
+  default     = ""
+}
+
+variable "ami_id" {
+  description = "AMI ID for the worker nodes. If not provided, the module will use the latest EKS-optimized AMI."
+  type        = string
+  default     = ""
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
