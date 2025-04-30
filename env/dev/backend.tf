fcd93381cd097670c20e3e374f8f3c452ea6a258
manivasagan <80743457+mani-bca@users.noreply.github.com>
2025-04-30 21:11:08 +0530

    Update backend.tf

--- env/dev/backend.tf
+++ env/dev/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-bucketsa"
-    key            = "eks/terraform.tfstate"
-    region         = "us-east-1"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket         = "terraform-state-bucketsa"
+#    key            = "eks/terraform.tfstate"
+#    region         = "us-east-1"
+#  }
+#}
