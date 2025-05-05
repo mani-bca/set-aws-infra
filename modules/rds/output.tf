c45b3c68aa2363e6ebc2a5f9d49722649205ba94
mani-bca <manidevops2021@gmail.com>
2025-05-05 22:13:31 +0530

    rds momduleG

--- modules/rds/output.tf
+++ modules/rds/output.tf
@@ -1,4 +1,50 @@
-output "rds_endpoint" {
-  value = aws_db_instance.mysql.endpoint
-  description = "Endpoint of the RDS instance"
+output "db_instance_id" {
+  description = "The RDS instance ID"
+  value       = aws_db_instance.this.id
+}
+
+output "db_instance_address" {
+  description = "The address of the RDS instance"
+  value       = aws_db_instance.this.address
+}
+
+output "db_instance_endpoint" {
+  description = "The connection endpoint of the RDS instance"
+  value       = aws_db_instance.this.endpoint
+}
+
+output "db_instance_arn" {
+  description = "The ARN of the RDS instance"
+  value       = aws_db_instance.this.arn
+}
+
+output "db_subnet_group_id" {
+  description = "The DB subnet group ID"
+  value       = aws_db_subnet_group.this.id
+}
+
+output "db_instance_name" {
+  description = "The database name"
+  value       = aws_db_instance.this.db_name
+}
+
+output "db_instance_username" {
+  description = "The master username for the database"
+  value       = aws_db_instance.this.username
+  sensitive   = true
+}
+
+output "db_instance_port" {
+  description = "The database port"
+  value       = aws_db_instance.this.port
+}
+
+output "db_instance_engine" {
+  description = "The database engine"
+  value       = aws_db_instance.this.engine
+}
+
+output "db_instance_engine_version" {
+  description = "The database engine version"
+  value       = aws_db_instance.this.engine_version
 }
