242609e6f860525be340bf0f0b291c16e50447d7
mani-bca <manidevops2021@gmail.com>
2025-05-05 22:20:34 +0530

    removed condition

--- modules/vpc/variable.tf
+++ modules/vpc/variable.tf
@@ -15,10 +15,10 @@ variable "availability_zones" {
   type        = list(string)
   default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
   
-  validation {
-    condition     = length(var.availability_zones) == 3
-    error_message = "You must provide exactly 3 availability zones."
-  }
+  # validation {
+  #   condition     = length(var.availability_zones) == 3
+  #   error_message = "You must provide exactly 3 availability zones."
+  # }
 }
 
 variable "public_subnet_cidrs" {
@@ -26,10 +26,10 @@ variable "public_subnet_cidrs" {
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   
-  validation {
-    condition     = length(var.public_subnet_cidrs) == 3
-    error_message = "You must provide exactly 3 CIDR blocks for public subnets."
-  }
+  # validation {
+  #   condition     = length(var.public_subnet_cidrs) == 3
+  #   error_message = "You must provide exactly 3 CIDR blocks for public subnets."
+  # }
 }
 
 variable "private_subnet_cidrs" {
@@ -37,10 +37,10 @@ variable "private_subnet_cidrs" {
   type        = list(string)
   default     = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
   
-  validation {
-    condition     = length(var.private_subnet_cidrs) == 3
-    error_message = "You must provide exactly 3 CIDR blocks for private subnets."
-  }
+  # validation {
+  #   condition     = length(var.private_subnet_cidrs) == 3
+  #   error_message = "You must provide exactly 3 CIDR blocks for private subnets."
+  # }
 }
 
 variable "create_nat_gateway" {
