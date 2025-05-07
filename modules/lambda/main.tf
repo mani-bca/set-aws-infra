d17725108d2551d90de967d625a6090330591117
manivasagan <80743457+mani-bca@users.noreply.github.com>
2025-05-07 17:10:13 +0000

    rfgh

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -24,3 +24,59 @@ resource "aws_lambda_function" "this" {
   # Add Lambda Layers for dependencies if needed
   layers = var.lambda_layers
 }
+
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.function_name}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = var.tags
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "${var.function_name}-policy"
+  description = "IAM policy for Lambda function ${var.function_name}"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = concat(
+      [
+        {
+          Action = [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ]
+          Effect   = "Allow"
+          Resource = "arn:aws:logs:*:*:*"
+        }
+      ],
+      var.additional_policy_statements
+    )
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
+
+resource "aws_lambda_permission" "s3_permission" {
+  count         = var.enable_s3_trigger ? 1 : 0
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.this.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = var.s3_bucket_arn
+}
