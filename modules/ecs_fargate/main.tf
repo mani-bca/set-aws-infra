d6eb7621a0514812f3c39d43354acef6a7a3540a
manivasagan <80743457+mani-bca@users.noreply.github.com>
2025-05-15 22:53:29 +0530

    Create ecs_fargate/main.tf

---
+++ modules/ecs_fargate/main.tf
@@ -0,0 +1,49 @@
+resource "aws_ecs_cluster" "this" {
+  name = var.cluster_name
+}
+
+resource "aws_ecs_task_definition" "this" {
+  family                   = var.task_family
+  network_mode            = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                     = var.cpu
+  memory                  = var.memory
+  execution_role_arn      = var.execution_role_arn
+  task_role_arn           = var.task_role_arn
+
+  container_definitions = jsonencode([
+    {
+      name      = var.container_name
+      image     = var.container_image
+      cpu       = var.cpu
+      memory    = var.memory
+      essential = true
+      portMappings = [
+        {
+          containerPort = var.container_port
+          hostPort      = var.container_port
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "this" {
+  name            = var.service_name
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.this.arn
+  desired_count   = var.desired_count
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = var.subnets
+    assign_public_ip = var.assign_public_ip
+    security_groups  = var.security_groups
+  }
+
+  lifecycle {
+    ignore_changes = [task_definition]
+  }
+
+  depends_on = [aws_ecs_task_definition.this]
+}
